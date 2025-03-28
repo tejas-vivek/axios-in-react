c1f0d426aac48df25b2fe6aaa03aed6fad95b757
tejas-vivek <tejas.helwatkar@gmail.com>
2025-03-27 23:45:37 -0400

    Axios with async function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
-import axios from "axios"
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    try {
+      const response = await axios.get("/products");
+      console.log(response);
+      setProducts(response.data.products);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
-    <div>App</div>
+    <>
+      {error != "" && error}
+      {products.map((product, index) => {
+        return (
+          <h3 key={index}>
+            {product.title} - ${product.price}
+          </h3>
+        );
+      })}
+    </>
   );
 }
 
